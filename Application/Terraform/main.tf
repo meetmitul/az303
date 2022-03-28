1d15ac708114b74eabc5bafa601cc3059d0c40a4
meetmitul <41762236+meetmitul@users.noreply.github.com>
2022-03-28 14:53:07 +1100

    Update main.tf

--- Application/Terraform/main.tf
+++ Application/Terraform/main.tf
@@ -61,7 +61,7 @@ resource "azurerm_app_service" "cicd_app" {
   app_service_plan_id = azurerm_app_service_plan.cicd_appplan.id
 
   site_config {
-    dotnet_framework_version = "v4.0"
-    use_32_bit_worker_process = true
+    use_32_bit_worker_process = true # this is only because using F1 SKU which doens't support 64bit
+    linux_fx_version = "DOTNETCORE|3.1"
   }
 }
