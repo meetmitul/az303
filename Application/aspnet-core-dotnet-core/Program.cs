920bf7a977e3b0f5c6489e309303013af7494dc8
Mitul Patel <mitul.patel@illion.com.au>
2024-11-15 09:24:41 +1100

    Timezone testing

--- Application/aspnet-core-dotnet-core/Program.cs
+++ Application/aspnet-core-dotnet-core/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,6 +17,21 @@ namespace aspnet_core_dotnet_core
     {
         public static void Main(string[] args)
         {
+            //System.Data.DataSet ds = new System.Data.DataSet();
+            //ds.Tables.Add("Table1");
+            //ds.Tables["Table1"].Columns.Add("Name");
+            //ds.Tables["Table1"].Rows.Add("Mitul");
+            //ds.Tables["Table1"].Rows.Add("Patel");
+            //ds.AcceptChanges();
+            ////make change
+            //ds.Tables["Table1"].Rows[1][0] = "NewPatel";
+            //MemoryStream s = new MemoryStream();
+            //ds.WriteXml(s,XmlWriteMode.IgnoreSchema);
+            //byte[] byteArray = s.ToArray();
+            //MemoryStream newstream = new MemoryStream(byteArray);
+            //XmlSerializer serialiser = new XmlSerializer(typeof(DataSet));
+            //Object newDS = (DataSet)serialiser.Deserialize(newstream);
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
